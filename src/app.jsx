0eed922e1a45f893d94a299be3c98ae9c81e0f61
Alex Romanak <alexromanak@Alexs-MacBook-Pro.local>
2018-06-01 16:18:24 -0700

    Bare-bones React Environment is set up

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+import "./app.css";
+
+class App extends Component{
+  render () {
+    return (
+      <div className="app">
+        <h1> App is here! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
