66e19f8907925cfc702df67c2b69dfe2abf8b9ee
Alex Romanak <alexromanak@Alexs-MBP.attlocal.net>
2018-06-03 15:16:21 -0700

    Refactored App from FSC to a class

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './app.css';
 
-const App = () => (
-  <div className="app">
-    <h1> App is here! </h1>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state({
+      example: 'example',
+    });
+  }
+  render() {
+    return (
+      <div className="app">
+        <h1> App is here! </h1>
+      </div>
+    );
+  }
+}
 
 export default App;
