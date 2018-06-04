d09088dcc366fe8f2d89b5b16b7f69b1c61fd1f6
Alex Romanak <alexromanak@Alexs-MBP.attlocal.net>
2018-06-04 11:50:49 -0700

    Created draft of function that will retrieve average star data

--- server/models/models.js
+++ server/models/models.js
@@ -1,1 +1,22 @@
+const db = require('../../database/index.js');
+const mysql = require('mysql');
+const Promise = require('bluebird');
 
+module.exports.listingAverageStars.get = (listingID) => {
+  return new Promise((resolve, reject) => {
+    // Using prepared statements to protect against SQL Injection Attacks
+    // See here: https://dev.mysql.com/doc/refman/5.7/en/sql-syntax-prepared-statements.html
+    // And here: https://github.com/mysqljs/mysql#preparing-queries
+    let starsQuery = 'SELECT rank_accuracy, rank_communication, rank_cleanliness, rank_location, rank_checkin, rank_value FROM ?? WHERE ?? = ?';
+    const inserts = ['review', 'listing_id', listingID];
+    starsQuery = mysql.format(starsQuery, inserts);
+
+    db.dbConnection.query(starsQuery, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
