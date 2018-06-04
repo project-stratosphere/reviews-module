8d0fdf59b1113b0872a7a2b0810f483a0787f5b2
Alex Romanak <alexromanak@Alexs-MBP.attlocal.net>
2018-06-04 12:10:32 -0700

    Fixed typo in review column header; now loading data from schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,5 @@
 -- //https://dev.mysql.com/doc/refman/5.7/en/
 
--- for reference: a command to load data into database:
--- load data local infile '/Users/...[PATH].../database/data/userdata.txt' into table user
-
 DROP DATABASE IF EXISTS airbnc_data;
 
 CREATE DATABASE airbnc_data;
@@ -22,7 +19,7 @@ CREATE TABLE review (
   review_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   listing_id INT NOT NULL,
   user_id INT NOT NULL,
-  reivew_date DATE,
+  review_date DATE,
   review_text VARCHAR(1500) NOT NULL,
   rank_accuracy TINYINT NOT NULL,
   rank_communication TINYINT NOT NULL,
@@ -32,11 +29,17 @@ CREATE TABLE review (
   rank_value TINYINT NOT NULL
 );
 
+-- INSERT INTO review VALUES (NULL, 1, 1, '2018-07-11', "Hello it's me", 5, 4, 5, 5, 5, 4);
+-- INSERT INTO review VALUES (NULL, 16, 1, '2016-03-19', "I like this house", 5, 4, 5, 5, 5, 4);
+-- INSERT INTO review VALUES (NULL, 8, 1, '2018-07-11', "What is up?", 5, 4, 5, 5, 5, 4);
+
 ALTER TABLE review ADD FOREIGN KEY (user_id) REFERENCES user (user_id);
 
--- INSERT INTO review VALUES (NULL, 1, 1, '2018-07-11', "Hello it's me", 5, 4, 5, 5, 5, 4);
--- INSERT INTO review VALUES (NULL, 16, 1, '2016-03-19', "I like stuff", 5, 4, 5, 5, 5, 4);
--- INSERT INTO review VALUES (NULL, 8, 1, '2018-07-11', "What is up?", 5, 4, 5, 5, 5, 4);
+-- for reference: https://dev.mysql.com/doc/refman/5.7/en/load-data.html
+-- take a look at the paragraphs that describe the LOCAL keyword 
+LOAD DATA LOCAL INFILE 'data/userdata.txt' into table user;
+LOAD DATA LOCAL INFILE 'data/reviewdata.txt' into table review;
+
 
 
 
