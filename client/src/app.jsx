dd72c73ef2fe1f578214ab4ef2bb391c623a841b
Alex Romanak <alexromanak@Alexs-MBP.attlocal.net>
2018-06-04 20:06:30 -0700

    Client state now populated by axios get request

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import axios from 'axios';
 import './app.css';
 
 import ContainerAverageStars from './components/ContainerAverageStars.jsx';
@@ -8,10 +9,56 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    this.getListingAverageStars = this.getListingAverageStars.bind(this);
+    this.getListingReviews = this.getListingReviews.bind(this);
+
+    // Just for testing
+    this.logState = this.logState.bind(this);
+
     this.state = {
-      example: 'example',
+      averageStarsObj: {},
+      reviews: [],
     };
   }
+
+  componentDidMount() {
+    // Using parameter 1 for now, just for testing
+    this.getListingReviews(1);
+    this.getListingAverageStars(1);
+  }
+
+  getListingAverageStars(id) {
+    axios.get(`/api/listings/${id}/averagestars`)
+      .then((response) => {
+        // console.log(response.data);
+        this.setState({
+          averageStarsObj: response.data,
+        });
+      })
+      .catch((error) => {
+        // console.log(error);
+      });
+  }
+
+  getListingReviews(id) {
+    axios.get(`/api/listings/${id}/reviews`)
+      .then((response) => {
+        // console.log(response.data);
+        this.setState({
+          reviews: response.data,
+        });
+      })
+      .catch((error) => {
+        // console.log(error);
+      });
+  }
+
+  // Just for testing.  Can drop into a div using the following tag: onClick={this.logState} 
+  logState() {
+    console.log(this.state.averageStarsObj);
+    console.log(this.state.reviews);
+  }
+
   render() {
     return (
       <div className="app">
--- client/src/components/ContainerAverageStars.jsx
+++ client/src/components/ContainerAverageStars.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import TotalReviews from './TotalReviews.jsx';
 
