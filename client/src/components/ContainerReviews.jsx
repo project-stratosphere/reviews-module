5b4f06822f5a4faf6a347d16a3dd03755420619a
Alex Romanak <alexromanak@Alexs-MBP.attlocal.net>
2018-06-06 17:27:46 -0700

    Added propTypes validation/defaults to ContainerReviews and ReviewEntry

--- client/src/components/ContainerReviews.jsx
+++ client/src/components/ContainerReviews.jsx
@@ -10,4 +10,22 @@ const ContainerReviews = props => (
   </div>
 );
 
+ContainerReviews.propTypes = {
+  reviews: PropTypes.arrayOf(PropTypes.shape({
+    review_date: PropTypes.string,
+    review_text: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+  })),
+};
+
+ContainerReviews.defaultProps = {
+  reviews: [{
+    review_date: '2016-11-04',
+    review_text: 'Laborum eius id rerum et in ratione',
+    first_name: 'Toby',
+    last_name: 'Keith',
+  }],
+};
+
 export default ContainerReviews;
--- client/src/components/ReviewEntry.jsx
+++ client/src/components/ReviewEntry.jsx
@@ -9,4 +9,22 @@ const ReviewEntry = props => (
   </div>
 );
 
+ReviewEntry.propTypes = {
+  review: PropTypes.shape({
+    review_date: PropTypes.string,
+    review_text: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+  }),
+};
+
+ReviewEntry.defaultProps = {
+  review: {
+    review_date: '2016-11-04',
+    review_text: 'Laborum eius id rerum et in ratione',
+    first_name: 'Toby',
+    last_name: 'Keith',
+  },
+};
+
 export default ReviewEntry;
