556086fc01722e41e0c14f0267a92f34dfb70c43
Alex Romanak <alexromanak@Alexs-MBP.attlocal.net>
2018-06-09 22:13:34 -0700

    Added dummy data to ReviewEntry test

--- __test__/ReviewEntry.test.jsx
+++ __test__/ReviewEntry.test.jsx
@@ -7,9 +7,18 @@ import ReviewEntry from '../client/src/components/ReviewEntry';
 
 configure({ adapter: new Adapter() });
 
+const dummyReview = {
+  review_date: '2017-02-12T08:00:00.000Z',
+  review_text: 'Debitis sequi id nam optio iste molestias dolores dolorem ex.',
+  first_name: 'Peggie',
+  last_name: 'Oberbrunner',
+};
+
+const dummyKey = 3;
+
 describe('ReviewEntry', () => {
   it('should render correctly', () => {
-    const output = shallow(<ReviewEntry />);
+    const output = shallow(<ReviewEntry key={dummyKey} review={dummyReview} />);
     expect(shallowToJson(output)).toMatchSnapshot();
   });
 });
