6329867859188d7fda106a808b9bced2955f8b47
Jonathan Pizzolato <jpizzolato36@gmail.com>
2018-06-25 11:48:36 -0700

    In this commit: Created the writeToDB script.

--- database/writeToDb.js
+++ database/writeToDb.js
@@ -9,19 +9,66 @@ const conObject = {
   port: 5432,
 };
 
-const filePath = path.resolve(__dirname, 'seed/users.csv');
-const pool = new Pool(conObject);
-const sql = `COPY tbl_users (first_name, last_name, user_name)  FROM '${filePath}' WITH DELIMITER '|' CSV HEADER;`;
-// const values;
 
-console.time('writeToDb');
-(async () => {
-  const client = await pool.connect();
-  try {
-    const res = await client.query(sql);
-    console.timeEnd('writeToDb');
-    console.log(res.rows[0]);
-  } finally {
-    client.release();
-  }
-})().catch(e => console.log(e.stack));
+const runUserLoad = false;
+const runListingLoad = false;
+const runReviewLoad = true;
+const pool = new Pool(conObject);
+// const values;
+let queryRuns = false
+
+if (runUserLoad) {
+  queryRuns = true;
+  let filePath = path.resolve(__dirname, 'seed/users.csv');
+  let sql = `COPY tbl_users (first_name, last_name, user_name)  FROM '${filePath}' WITH DELIMITER '|' CSV HEADER;`;
+
+  console.time('writeToDb');
+  (async () => {
+    const client = await pool.connect();
+    try {
+      const res = await client.query(sql);
+      console.timeEnd('writeToDb');
+      console.log(res.rows[0]);
+    } finally {
+      client.release();
+    }
+  })().catch(e => console.log(e.stack));
+}
+
+if (runListingLoad) {
+  queryRuns = true;
+  let filePath = path.resolve(__dirname, 'seed/listingIds.csv');
+  let sql = `COPY tbl_listings (listing_name)  FROM '${filePath}' WITH DELIMITER '|' CSV HEADER;`;
+
+  console.time('writeToDb');
+  (async () => {
+    const client = await pool.connect();
+    try {
+      const res = await client.query(sql);
+      console.timeEnd('writeToDb');
+      console.log(res.rows[0]);
+    } finally {
+      client.release();
+    }
+  })().catch(e => console.log(e.stack));
+}
+
+if (runReviewLoad) {
+  queryRuns = true;
+  let filePath = path.resolve(__dirname, 'seed/reviewComments.csv');
+  let sql = `COPY tbl_reviews (listing_id, user_id, first_name, review_date, review_text, rank_accuracy, rank_communication, rank_cleanliness, rank_location, rank_checkin, rank_value)  FROM '${filePath}' WITH DELIMITER '|' CSV HEADER;`;
+
+  console.time('writeToDb');
+  (async () => {
+    const client = await pool.connect();
+    try {
+      const res = await client.query(sql);
+      console.timeEnd('writeToDb');
+      console.log(res.rows[0]);
+    } finally {
+      client.release();
+    }
+  })().catch(e => console.log(e.stack));
+}
+
+console.log('writing to db: ', queryRuns);
