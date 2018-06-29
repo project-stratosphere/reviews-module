e4c7541b64f4173aa6c25e1a64194f80d252aab1
Jonathan Pizzolato <jpizzolato36@gmail.com>
2018-06-29 16:40:44 -0700

    In this commit: reconfigured DB connection from single client to pool.

--- database/index.js
+++ database/index.js
@@ -1,9 +1,8 @@
-const mysql = require('mysql');
-const { Client } = require('pg');
+const { Client, Pool } = require('pg');
 
 // const { Pool, Client } = require('pg');
 // const pool = new Pool(conObject);
-const pgConnection = new Client({
+const pgConnection = new Pool({
   user: 'jonathanpizzolato',
   host: 'localhost',
   database: 'dev_airbnb_sdc',
@@ -18,19 +17,6 @@ pgConnection.connect((err) => {
   console.log('PostgreSQL connected');
 });
 
-const dbConnection = mysql.createConnection({
-  user: 'root',
-  database: 'dev_airbnb_sdc',
-});
-
-dbConnection.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log('mysql connected');
-});
-
 module.exports = {
-  dbConnection,
   pgConnection,
 };
